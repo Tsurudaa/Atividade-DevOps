a4a7e8af02c2f880c48d77a5343f8901b8c98ff2
Tsuruda <igortsugon@gmail.com>
2026-09-14 17:47:39 -0300

    test: adiciona testes da calculadora

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+from calculator import somar, subtrair, multiplicar, dividir
+
+
+def test_somar():
+    assert somar(2, 3) == 5
+
+
+def test_subtrair():
+    assert subtrair(5, 3) == 2
+
+
+def test_multiplicar():
+    assert multiplicar(4, 3) == 12
+
+
+def test_dividir():
+    assert dividir(10, 2) == 5
